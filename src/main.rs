994127fcb77142219cfb3a980ede43a60d1ff20b
Andrew Kujtan <Andrew.Kujtan@scotiabank.com>
2017-12-01 16:46:25 -0500

    use a and b.

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,10 @@ use day1::day1;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let data: String = input(&args[1]);
-    let (a1, a2) = match args[1].parse::<u32>().unwrap() {
+    let (a, b) = match args[1].parse::<u32>().unwrap() {
         1 => (day1(&data, ProblemPart::A), day1(&data, ProblemPart::B)),
         _ => (String::from("NULL"), String::from("NULL")),
     };
-    println!("Part A: {}", a1);
-    println!("Part B: {}", a2);
+    println!("Part A: {}", a);
+    println!("Part B: {}", b);
 }
