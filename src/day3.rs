6ce3c4d2b748a57f4385df859c1a91512bec0503
Andrew Kujtan <andrew@kujtan.com>
2017-12-04 08:06:39 -0500

    day 3

---
+++ src/day3.rs
@@ -0,0 +1,127 @@
+use aoc::*;
+
+pub fn day3(data: &String, _prob: ProblemPart) -> String {
+    let num = data.parse::<i32>().unwrap();
+    match _prob {
+        ProblemPart::A => {
+            let mut cnt = 0;
+            let mut low = 1;
+            let mut high = 1;
+            while high < num {
+                cnt = cnt + 1;
+                low = high + 1;
+                high = high + (8 * cnt);
+            }
+            let side = (high - low + 1) / 4;
+            let index = match num - low {
+                0 => 0,
+                _ => (num - low) % side,
+            };
+            let mid = match side {
+                0 => 0,
+                _ => (side / 2) - 1,
+            };
+            let step = match index {
+                index if index < mid => mid - index,
+                _ => index - mid,
+            };
+            (cnt as i32 + step).to_string()
+        },
+        ProblemPart::B => {
+            let mut n =  [[0i32; 100]; 100];
+            let (mut x, mut y) = (49, 50);
+            n[y][x] = 1;
+            n[y][x - 1] = 1;
+            let mut dir = 1;
+            while n[y][x] <= num {
+                // println!("{},{}\n{},{},{}\n{},{},{}\n{},{},{}", x, y,
+                // n[y - 1][x - 1], n[y - 1][x], n[y - 1][x + 1],
+                // n[y][x - 1], n[y][x], n[y][x + 1],
+                // n[y + 1][x - 1], n[y + 1][x], n[y + 1][x + 1]);
+                match dir {
+                    0 => {x = x + 1;},
+                    1 => {y = y - 1;},
+                    2 => {x = x - 1;},
+                    _ => {y = y + 1;},
+                }
+                let should_turn = match dir {
+                    0 => n[y - 1][x] == 0,
+                    1 => n[y][x - 1] == 0,
+                    2 => n[y + 1][x] == 0,
+                    _ => n[y][x + 1] == 0,
+                };
+                if should_turn {
+                    dir = (dir + 1) % 4;
+                }
+                //Generate next num
+                n[y][x] = n[y - 1][x - 1] + n[y - 1][x] + n[y - 1][x + 1] +
+                n[y][x - 1] + n[y][x] + n[y][x + 1] +
+                n[y + 1][x - 1] + n[y + 1][x] + n[y + 1][x + 1];
+            }
+            n[y][x].to_string()
+        },
+    }
+
+
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!("0", day3(&String::from("1"), ProblemPart::A));
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!("3", day3(&String::from("12"), ProblemPart::A));
+    }
+
+    #[test]
+    fn test3() {
+        assert_eq!("2", day3(&String::from("23"), ProblemPart::A));
+    }
+
+    #[test]
+    fn test4() {
+        assert_eq!("31", day3(&String::from("1024"), ProblemPart::A));
+    }
+
+    #[test]
+    fn test5() {
+        assert_eq!("475", day3(&String::from("277678"), ProblemPart::A));
+    }
+
+    #[test]
+    fn test6() {
+        assert_eq!("1", day3(&String::from("2"), ProblemPart::A));
+    }
+
+    #[test]
+    fn test7() {
+        assert_eq!("3", day3(&String::from("16"), ProblemPart::A));
+    }
+
+    #[test]
+    fn test8() {
+        assert_eq!("2", day3(&String::from("1"), ProblemPart::B));
+    }
+    #[test]
+    fn test9() {
+        assert_eq!("4", day3(&String::from("2"), ProblemPart::B));
+    }
+    #[test]
+    fn test10() {
+        assert_eq!("10", day3(&String::from("5"), ProblemPart::B));
+    }
+    #[test]
+    fn test11() {
+        assert_eq!("23", day3(&String::from("22"), ProblemPart::B));
+    }
+    #[test]
+    fn test12() {
+        assert_eq!("806", day3(&String::from("750"), ProblemPart::B));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ extern crate aoc;
 
 mod day1;
 mod day2;
+mod day3;
 
 use std::env;
 use aoc::*;
 use day1::day1;
 use day2::day2;
+use day3::day3;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,6 +16,7 @@ fn main() {
     let (a, b) = match args[1].parse::<u32>().unwrap() {
         1 => (day1(&data, ProblemPart::A), day1(&data, ProblemPart::B)),
         2 => (day2(&data, ProblemPart::A), day2(&data, ProblemPart::B)),
+        3 => (day3(&data, ProblemPart::A), day3(&data, ProblemPart::B)),
         _ => (String::from("NULL"), String::from("NULL")),
     };
     println!("Part A: {}", a);
