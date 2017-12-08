2bd93b168d28aa56fc5d7268c60d0cd9189a597c
Andrew Kujtan <Andrew.Kujtan@scotiabank.com>
2017-12-08 12:45:34 -0500

    Day 7, RIP

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -1,6 +1,6 @@
 use aoc::*;
 use days::ChristmasDay;
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 
 pub struct Day7;
 
@@ -12,6 +12,11 @@ struct Node {
     children: Vec<String>,
     cweight: i32
 }
+impl Node {
+    fn fullweight(&self) -> i32 {
+        self.weight + (self.cweight * self.children.len() as i32)
+    }
+}
 
 impl ChristmasDay for Day7 {
     fn solve(&self, data: &str, prob: ProblemPart) -> String {
@@ -20,8 +25,6 @@ impl ChristmasDay for Day7 {
         data.lines().for_each(|line| {
             let nodes: Vec<String> = line.split_whitespace().map(String::from).collect::<Vec<String>>();
             let mut nodes_iter = nodes.iter();
-            // "xhth (57)"
-            // "fwft (72) -> ktlj, cntj, xhth"
             let pname = nodes_iter.next().unwrap().to_string();
             let pweight = nodes_iter.next().unwrap();
             let weight = pweight.chars().skip(1).take(pweight.len() - 2).collect::<String>().parse::<i32>().unwrap();
@@ -53,28 +56,52 @@ impl ChristmasDay for Day7 {
 
             if let Some(parent) = tree.get_mut(&pname) {
                 parent.children.extend(children);
-                parent.cweight += children_weight;
             }
         });
 
-        println!("Tree: --------------------");
-        for n in &tree {
-            println!("{:?}", n);
-        }
-        let root: &String = &tree.values().filter(|n| n.parent == None).last().unwrap().name;
-
         match prob {
-            ProblemPart::A => root.to_string(),
+            ProblemPart::A => tree.values().filter(|n| n.parent == None).last().unwrap().name.to_string(),
             ProblemPart::B => {
-                // let candidates: Vec<(String, i32)> = tree.iter().map(|n| (n.parent.unwrap(), n.weight)).collect();
-                //
-                // candidates.iter().for_each(|n| {
-                //     let mut parent = tree.iter().find(|p| p.name == n.0).unwrap();
-                //
-                // });
-                // println!("Err: {:?}", candidates);
-                
-                "".to_string()
+                let mut queue: VecDeque<String> = VecDeque::new();
+                let mut err_node: Option<String> = None;
+                tree.iter().filter(|n| n.1.children.len() == 0).for_each(|n| {
+                    queue.push_back(n.0.clone());
+                });
+
+                while !queue.is_empty() {
+                    let mut parentid: Option<String> = None;
+                    let mut weight: Option<i32> = None;
+                    if let Some(node) = tree.get_mut(&queue.pop_front().unwrap()) {
+                        // Queue the parent
+                        if let Some(parent) = node.parent.clone() {
+                            if !queue.contains(&parent) {
+                                queue.push_back(parent.clone());
+                            }
+                        }
+                        parentid = node.parent.clone();
+                        weight = Some(node.fullweight());
+                    }
+                    if let Some(node) = tree.get_mut(&parentid.unwrap()) {
+                        if node.cweight == 0 {
+                            node.cweight = weight.unwrap();
+                        } else if node.cweight != weight.unwrap(){
+                            queue.clear();
+                            err_node = Some(node.name.clone());
+                        }
+                    }
+                }
+
+                let node = tree.get(&err_node.unwrap()).unwrap();
+                let mut grouped: HashMap<i32, i32> = HashMap::new();
+                let nodes = node.children.iter().map(|n| tree.get(n).unwrap()).collect::<Vec<&Node>>();
+                nodes.iter().for_each(|child| {
+                        let e = grouped.entry(child.fullweight()).or_insert(0);
+                        *e += 1;
+                    });
+                let wrong = grouped.iter().min_by(|x, y| x.1.cmp(y.1)).unwrap().0;
+                let right = grouped.iter().max_by(|x, y| x.1.cmp(y.1)).unwrap().0;
+                let wrong_node = nodes.iter().find(|child| child.fullweight() == *wrong).unwrap();
+                (wrong_node.weight - (wrong - right)).to_string()
             },
         }
 
@@ -103,6 +130,6 @@ ugml (68) -> gyxo, ebii, jptl
 gyxo (61)
 cntj (57)";
         assert_eq!("tknk", Day7.solve_a(t));
-        assert_eq!("8", Day7.solve_b(t));
+        assert_eq!("60", Day7.solve_b(t));
     }
 }
