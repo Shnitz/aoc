aeaaa81c63c31a80301cdf7c8f2efb774e04cfbd
Andrew Kujtan <Andrew.Kujtan@scotiabank.com>
2017-12-12 12:40:51 -0500

    Day 12 complete.

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,11 +1,32 @@
+extern crate petgraph;
+
 use aoc::*;
 use days::ChristmasDay;
+use self::petgraph::visit::{ Bfs, Walker };
+use std::collections::HashMap;
 
 pub struct Day12;
 
 impl ChristmasDay for Day12 {
-    fn solve(&self, _data: &str, _prob: ProblemPart) -> String {
-        "".to_string()
+    fn solve(&self, data: &str, prob: ProblemPart) -> String {
+        let mut graph = petgraph::Graph::<&str, &str, petgraph::Undirected>::default();
+        let mut nodes: HashMap<&str, petgraph::graph::NodeIndex> = HashMap::new();
+        nodes.insert("0", graph.add_node("0"));
+        data.lines().for_each(|line| {
+            let mut params = line.split_whitespace();
+            let id = params.next().unwrap();
+            nodes.entry(id).or_insert_with(|| graph.add_node(id));
+            params.skip(1).for_each(|n| {
+                let tid = n.trim_matches(',');
+                nodes.entry(tid).or_insert_with(|| graph.add_node(tid));
+                graph.update_edge(*nodes.get(id).unwrap(), *nodes.get(tid).unwrap(), "");
+            });
+        });
+
+        match prob {
+            ProblemPart::A => Bfs::new(&graph, *nodes.get("0").unwrap()).iter(&graph).count().to_string(),
+            ProblemPart::B => petgraph::algo::connected_components(&graph).to_string()
+        }
     }
 }
 
@@ -15,7 +36,14 @@ mod test {
 
     #[test]
     fn day12_test1() {
-        assert_eq!("", Day12.solve_a(""));
-        assert_eq!("", Day12.solve_b(""));
+        let test_str = "0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5";
+        assert_eq!("6", Day12.solve_a(test_str));
+        assert_eq!("2", Day12.solve_b(test_str));
     }
 }
