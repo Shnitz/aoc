b767d27ce692d280a9805823200ff84cc3dc45f3
Andrew Kujtan <Andrew.Kujtan@scotiabank.com>
2017-12-13 10:33:46 -0500

    Day 13 complete.

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -4,8 +4,41 @@ use days::ChristmasDay;
 pub struct Day13;
 
 impl ChristmasDay for Day13 {
-    fn solve(&self, _data: &str, _prob: ProblemPart) -> String {
-        "".to_string()
+    fn solve(&self, data: &str, prob: ProblemPart) -> String {
+        let firewall = data.lines()
+            .map(str::split_whitespace)
+            .map(|mut i| (i.next().unwrap().trim_matches(':').parse::<u32>().unwrap(), i.next().unwrap().parse::<u32>().unwrap()))
+            .collect::<Vec<(u32, u32)>>();
+
+        match prob {
+            ProblemPart::A => firewall.iter().map(self::firewall_cost)
+                                    .sum::<u32>()
+                                    .to_string(),
+            ProblemPart::B => {
+                let mut delay = 0;
+                let mut zero_time = 1;
+                while zero_time != 0 {
+                    delay += 1;
+                    zero_time = firewall.iter()
+                                .map(|p| firewall_cost(&(p.0 + delay, p.1)))
+                                .sum::<u32>();
+                }
+                delay.to_string()
+            }
+        }
+
+    }
+}
+
+fn firewall_cost(&(time, size): &(u32, u32)) -> u32 {
+    assert!(size > 0);
+    if match size {
+        1 => true,
+        _ => time % (2 * (size - 1)) == 0,
+    } {
+        size * time
+    } else {
+        0
     }
 }
 
@@ -15,7 +48,11 @@ mod test {
 
     #[test]
     fn day13_test1() {
-        assert_eq!("", Day13.solve_a(""));
-        assert_eq!("", Day13.solve_b(""));
+        let data = "0: 3
+1: 2
+4: 4
+6: 4";
+        assert_eq!("24", Day13.solve_a(data));
+        assert_eq!("10", Day13.solve_b(data));
     }
 }
