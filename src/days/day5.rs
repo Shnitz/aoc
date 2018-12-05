63c8c3d75fe72e7548611f44c78c389348981b9f
Kujtan, Andrew <andrew.kujtan@scotiabank.com>
2018-12-05 11:41:19 -0500

    Complete day 5.

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -1,11 +1,43 @@
 use aoc::*;
 use days::ChristmasDay;
+use std::cmp;
 
 pub struct Day5;
 
+fn eq(l: char, r: char) -> bool {
+    if l.is_lowercase() { l.to_ascii_uppercase() == r } else { l.to_ascii_lowercase() == r }
+}
+
+fn react(mut pol: Vec<char>) -> usize {
+    let mut idx = 0;
+    while idx + 1 < pol.len() {
+        if eq(pol[idx], pol[idx + 1]) {
+            pol.drain(idx..idx + 2);
+            idx = if idx == 0 { 0 } else { idx - 1 };
+        } else {
+            idx += 1;
+        }
+    }
+    pol.len()
+}
+
 impl ChristmasDay for Day5 {
-    fn solve(&self, _data: &str, _prob: ProblemPart) -> String {
-        "".to_string()
+    fn solve(&self, data: &str, prob: ProblemPart) -> String {
+        let pol = data.chars().collect::<Vec<char>>();
+
+        match prob {
+            ProblemPart::A => react(pol).to_string(),
+            ProblemPart::B => {
+                (b'A'..=b'z')
+                    .filter_map(|c| {
+                        let c = c as char;
+                        if c.is_alphabetic() { Some(c) } else { None }
+                    }).fold(pol.len(), |min, rem| {
+                    let filtered = pol.iter().cloned().filter(|&c| c.to_ascii_lowercase() != rem).collect::<Vec<char>>();
+                    cmp::min(min, react(filtered))
+                }).to_string()
+            }
+        }
     }
 }
 
@@ -15,7 +47,7 @@ mod test {
 
     #[test]
     fn day24_test1() {
-        assert_eq!("", Day5.solve_a(""));
-        assert_eq!("", Day5.solve_b(""));
+        assert_eq!("10", Day5.solve_a("dabAcCaCBAcCcaDA"));
+        assert_eq!("4", Day5.solve_b("dabAcCaCBAcCcaDA"));
     }
 }
