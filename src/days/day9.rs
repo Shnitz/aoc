24261e10946ddda831070a3be2067c609a5e45f4
Kujtan, Andrew <andrew.kujtan@scotiabank.com>
2018-12-11 10:57:07 -0500

    Complete day 9, part 2

--- src/days/day9.rs
+++ src/days/day9.rs
@@ -1,5 +1,6 @@
 use aoc::*;
 use days::ChristmasDay;
+use std::collections::VecDeque;
 
 pub struct Day9;
 
@@ -12,21 +13,25 @@ impl ChristmasDay for Day9 {
             ProblemPart::B => game_info[1] * 100
         };
 
-        let mut board = vec![0];
+        let mut board = VecDeque::with_capacity(num_marbles);
+        board.push_front(0);
         let mut score = vec![0usize; players];
-        let mut cur_marble = 0;
         let mut cur_elf = 0;
 
         for marble in 1..=num_marbles {
             if marble % 23 == 0 {
-                cur_marble = (cur_marble + board.len() - 7) % board.len();
-                let rem = board.remove(cur_marble);
+                for _ in 0..7 {
+                    let el = board.pop_back().unwrap();
+                    board.push_front(el);
+                }
+                let rem = board.pop_front().unwrap();
                 score[cur_elf] += marble + rem;
-
-                println!("-> {} score !{}! {}", marble - 4, rem, rem + marble);
             } else {
-                cur_marble = ((cur_marble + 1) % board.len()) + 1;
-                board.insert(cur_marble, marble);
+                for _ in 0..=1 {
+                    let el = board.pop_front().unwrap();
+                    board.push_back(el);
+                }
+                board.push_front(marble);
             }
 
             cur_elf = (cur_elf + 1) % players;
@@ -41,16 +46,14 @@ mod test {
 
     #[test]
     fn day9_test1() {
-        assert_eq!("32", Day9.solve_a("9 players; last marble is worth 1000 points"));
+        assert_eq!("32", Day9.solve_a("9 players; last marble is worth 25 points"));
+        assert_eq!("8317", Day9.solve_a("10 players; last marble is worth 1618 points"));
+        assert_eq!("2764", Day9.solve_a("17 players; last marble is worth 1104 points"));
+        assert_eq!("54718", Day9.solve_a("21 players; last marble is worth 6111 points"));
+        assert_eq!("37305", Day9.solve_a("30 players; last marble is worth 5807 points"));
+        assert_eq!("146373", Day9.solve_a("13 players; last marble is worth 7999 points"));
+        assert_eq!("370210", Day9.solve_a("464 players; last marble is worth 70918 points"));
 
-//        assert_eq!("32", Day9.solve_a("9 players; last marble is worth 25 points"));
-//        assert_eq!("8317", Day9.solve_a("10 players; last marble is worth 1618 points"));
-//        assert_eq!("2764", Day9.solve_a("17 players; last marble is worth 1104 points"));
-//        assert_eq!("54718", Day9.solve_a("21 players; last marble is worth 6111 points"));
-//        assert_eq!("37305", Day9.solve_a("30 players; last marble is worth 5807 points"));
-//        assert_eq!("146373", Day9.solve_a("13 players; last marble is worth 7999 points"));
-//        assert_eq!("370210", Day9.solve_a("464 players; last marble is worth 70918 points"));
-
-//        assert_eq!("370210", Day9.solve_b("464 players; last marble is worth 70918 points"));
+        assert_eq!("3101176548", Day9.solve_b("464 players; last marble is worth 70918 points"));
     }
 }
